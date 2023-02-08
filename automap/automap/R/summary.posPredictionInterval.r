090c4fd1a2ef3c1410baf41d5cbc16e6dd32dba4
Jon Skoien <skoiejo@ies.jrc.it>
2023-02-08 18:45:10 +0100

    changed structure of automatp

--- automap/automap/R/summary.posPredictionInterval.r
+++
@@ -1,8 +0,0 @@
-summary.posPredictionInterval = function(object, ...)
-{
-    cat("Upper and lower boundary and the position relative to value:")
-    print(summary(object$pos_prediction), ...)
-    cat("\n")
-    print(object["p"], ...)
-    print(object["value"], ...)
-}
